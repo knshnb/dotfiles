5deb39d10ac80eb3844891545fe3f19344bb4b1a
Kenshin Abe <abe.kenshin@gmail.com>
2018-04-09 15:22:45 +0900

    Add files for oh-my-zsh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/dotfiles/.vim ~/.vim
 ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
+ln -s ~/dotfiles/.zshrc ~/.zshrc
