4563415cdeb972b365613074b1e696901096142b
Kenshin Abe <abe.kenshin@gmail.com>
2018-08-25 20:45:54 +0900

    Update competitive template

--- .competitive_template.cpp
+++ .competitive_template.cpp
@@ -2,15 +2,16 @@
 #define ll long long
 #define REP(i, n) for (ll (i) = 0; (i) < (n); (i)++)
 #define REPI(i, a, b) for (ll (i) = (a); (i) < (b); (i)++)
-#define P pair<ll, ll>
-#define VI vector<ll>
 #define int long long
 using namespace std;
+using P = pair<int, int>;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VVVI = vector<VVI>;
 
 void solve() {
 }
 
 signed main() {
-
   solve();
 }
