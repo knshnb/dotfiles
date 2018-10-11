7bcf8a144f62bd31f823ac8be9ef49c0ff2f6549
Kenshin Abe <kenshin.abe@linecorp.com>
2018-10-11 11:52:50 +0900

    Add .ideavimrc

--- setup.sh
+++ setup.sh
@@ -5,3 +5,4 @@ ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/dotfiles/.vim ~/.vim
 ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
 ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.ideavimrc ~/.ideavimrc
