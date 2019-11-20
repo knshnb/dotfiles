96f09300a88a5e6d29c59d41d45ddfcc67f95e16
Kenshin Abe <abe.kenshin@gmail.com>
2019-11-20 17:52:44 +0900

    Generate VSCode snippets

--- setup.sh
+++ setup.sh
@@ -7,7 +7,9 @@ ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.ideavimrc ~/.ideavimrc
 ln -s ~/dotfiles/settings.json ~/Library/Application\ Support/Code/User/settings.json
-ln -s ~/dotfiles/keybindings.json ~//Library/Application\ Support/Code/User/keybindings.json
+ln -s ~/dotfiles/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 ln -s ~/dotfiles/settings.json ~/Library/Application\ Support/Code\ -\ Insiders/User/settings.json
-ln -s ~/dotfiles/keybindings.json ~//Library/Application\ Support/Code\ -\ Insiders/User/keybindings.json
+ln -s ~/dotfiles/keybindings.json ~/Library/Application\ Support/Code\ -\ Insiders/User/keybindings.json
+ln -s ~/dotfiles/cpp.json ~/Library/Application\ Support/Code/User/snippets/cpp.json
+ln -s ~/dotfiles/cpp.json ~/Library/Application\ Support/Code\ -\ Insiders/User/snippets/cpp.json
 ln -s ~/dotfiles/.competitive_template.cpp ~/.competitive_template.cpp
