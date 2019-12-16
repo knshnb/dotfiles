5cfae6f69bbb38e9363268d91cd48d436dc00c0c
Kenshin Abe <abe.kenshin@gmail.com>
2019-12-15 18:20:15 -0800

    Add to_string for bitset

--- .competitive_local.hpp
+++ .competitive_local.hpp
@@ -1,9 +1,15 @@
-#include <bits/stdc++.h>
 using namespace std;
 
-string to_string(const string& s) { return '"' + s + '"'; }
-string to_string(const char* s) { return to_string((string)s); }
+string to_string(const string &s) { return '"' + s + '"'; }
+string to_string(const char *s) { return to_string((string)s); }
 string to_string(bool b) { return (b ? "true" : "false"); }
+template <size_t N> string to_string(bitset<N> v) {
+    string res = "";
+    for (size_t i = 0; i < N; i++) {
+        res += static_cast<char>('0' + v[i]);
+    }
+    return res;
+}
 template <class A, class B> string to_string(pair<A, B> p) {
     return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
 }
@@ -13,7 +19,7 @@ template <class A, class B, class C> string to_string(tuple<A, B, C> p) {
 template <class A> string to_string(A v) {
     bool first = true;
     string res = "{";
-    for (const auto& x : v) {
+    for (const auto &x : v) {
         if (!first) res += ", ";
         first = false, res += to_string(x);
     }
@@ -21,7 +27,7 @@ template <class A> string to_string(A v) {
 }
 
 void dump_func() { cerr << endl; }
-template <class Head, class... Tail> void dump_func(Head&& h, Tail&&... t) {
+template <class Head, class... Tail> void dump_func(Head &&h, Tail &&... t) {
     cerr << to_string(h) << (sizeof...(Tail) == 0 ? "" : ", "), dump_func(forward<Tail>(t)...);
 }
 
