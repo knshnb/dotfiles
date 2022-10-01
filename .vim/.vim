2b4e262d7344f616346322374e03ba4ad7c161c0
Kenshin Abe <abe.kenshin@gmail.com>
2022-10-01 23:10:10 +0900

    Delete unnecessary files

--- .vim/.vim
+++
@@ -1,1 +0,0 @@
-/home/tansei/dotfiles/.vim
--- setup.sh
+++ setup.sh
@@ -1,16 +1,9 @@
 #! /bin/bash
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.bashrc ~/.bashrc
-ln -s ~/dotfiles/.bash_profile ~/.bash_profile
-ln -s ~/dotfiles/.vim ~/.vim
+# ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
 ln -s ~/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/.ideavimrc ~/.ideavimrc
-ln -s ~/dotfiles/settings.json ~/Library/Application\ Support/Code/User/settings.json
-ln -s ~/dotfiles/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
-ln -s ~/dotfiles/settings.json ~/Library/Application\ Support/Code\ -\ Insiders/User/settings.json
-ln -s ~/dotfiles/keybindings.json ~/Library/Application\ Support/Code\ -\ Insiders/User/keybindings.json
-ln -s ~/dotfiles/cpp.json ~/Library/Application\ Support/Code/User/snippets/cpp.json
-ln -s ~/dotfiles/cpp.json ~/Library/Application\ Support/Code\ -\ Insiders/User/snippets/cpp.json
+ln -s ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -s ~/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+ln -s ~/dotfiles/vscode/cpp.json ~/Library/Application\ Support/Code/User/snippets/cpp.json
 ln -s ~/dotfiles/.competitive_template.cpp ~/.competitive_template.cpp
 ln -s ~/dotfiles/.competitive_local.hpp ~/.competitive_local.hpp
